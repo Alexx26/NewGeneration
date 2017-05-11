187dee8a2f7a4f5b66bf73e000cc6f7dfb19ed1e
Alex Bolilyi <alex.bolilyi@gmail.com>
2017-05-11 23:19:03 +0300

    Add the "PupilContext.cs" file.

---
+++ NewGeneration/Models/PupilContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace NewGeneration.Models
+{
+    public class PupilContext : DbContext
+    {
+        public DbSet<Pupil> Pupils { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+    }
+
+}

8db2bcfba1be1de965d5b80dee2f37c23fe3cf00
Alex Bolilyi <alex.bolilyi@gmail.com>
2017-05-11 23:02:28 +0300

    Add the "Parent.cs" file to the project.

---
+++ NewGeneration/Models/Parent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewGeneration.Models
+{
+    public class Parent
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public int Phone { get; set; }
+        public int PupilId { get; set; }
+    }
+}

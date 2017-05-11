2bd83eb341a6a853c9ac47c3b3c5e793af162389
Alex Bolilyi <alex.bolilyi@gmail.com>
2017-05-11 22:47:41 +0300

    Add project files.

---
+++ NewGeneration/Models/Pupil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewGeneration.Models
+{
+    public class Pupil
+    {
+        public int Id { get; set; }
+    }
+}

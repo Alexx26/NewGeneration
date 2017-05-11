a7a7e37d4c16e33ec699cca8721cd7fc5c4a320a
Alex Bolilyi <alex.bolilyi@gmail.com>
2017-05-11 22:48:55 +0300

    First commit into the project.

--- NewGeneration/Models/Pupil.cs
+++ NewGeneration/Models/Pupil.cs
@@ -8,5 +8,9 @@ namespace NewGeneration.Models
     public class Pupil
     {
         public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Phone { get; set; }
     }
 }

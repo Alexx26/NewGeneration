aab198e29fa61c02823fdcd9c467298e693fd007
Alex Bolilyi <alex.bolilyi@gmail.com>
2017-05-12 22:59:14 +0300

    Update the "Global.asax.cs" file.

--- NewGeneration/Global.asax.cs
+++ NewGeneration/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NewGeneration.Models;
+using System.Data.Entity;
 
 namespace NewGeneration
 {
@@ -12,6 +14,8 @@ namespace NewGeneration
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new PupilDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);

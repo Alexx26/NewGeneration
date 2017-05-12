2e87d164097499cd506c94f41c614c8f3b495246
Alex Bolilyi <alex.bolilyi@gmail.com>
2017-05-12 22:56:35 +0300

    Add the class "PupilDbInitializer.cs".

---
+++ NewGeneration/Models/PupilDbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace NewGeneration.Models
+{
+    public class PupilDbInitializer : DropCreateDatabaseIfModelChanges<PupilContext>
+    {
+        protected override void Seed(PupilContext context)
+        {
+            context.Pupils.Add(new Pupil { Surname = "Andrievskyi", Name = "Yaroslav", Birthday = new DateTime(2000, 03, 08), Phone = 0637362925});
+            context.Pupils.Add(new Pupil { Surname = "Anikina", Name = "Kateryna", Birthday = new DateTime(2000, 04, 08), Phone = 0999149332 });
+            context.Pupils.Add(new Pupil { Surname = "Arkusha", Name = "Oleksii", Birthday = new DateTime(2000, 03, 30), Phone = 0686508499 });
+
+            base.Seed(context);
+        }
+    }
+
+}
